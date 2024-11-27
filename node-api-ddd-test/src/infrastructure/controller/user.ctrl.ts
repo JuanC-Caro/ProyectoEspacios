af24b6832ad296fe482650d8e850dc4d8aaceb8c
douglasd11 <dguerreroe@unicartagena.edu.co>
2024-11-27 13:23:56 -0500

    rol

--- node-api-ddd-test/src/infrastructure/controller/user.ctrl.ts
+++ node-api-ddd-test/src/infrastructure/controller/user.ctrl.ts
@@ -49,8 +49,8 @@ export class UserController {
       return;
     }
 
-    const { uuid, name, rol } = user;
-    const token = generateToken({ uuid, name, email, rol });
+    const { uuid, name, role } = user;
+    const token = generateToken({ uuid, name, email, role });
 
     res.status(200).send({ token, user });
   }
